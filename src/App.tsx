8b13b4150aab4f974230f5d56e07a62965e042af
PovilasGe <povilasgedminas@gmail.com>
2023-01-24 12:59:19 +0200

    add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
+
+export type Country =  {
+  name: string;
+  region: string;
+  area: number;
+}
+
+const countriesCountPerPage = 10;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [currentySelectedPageIndex, setCurrentlySelectedPageIndex] = useState(0);
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [loaded, setLoaded] = useState(false);
+  const [errored, setErrored] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data: countriesResponseData}= await axios.get<Country[]>("https://restcountries.com/v2/all?fields=name,region,area");
+        setCountries(countriesResponseData);
+      } catch {
+        setErrored(true);
+      } finally {
+        setLoaded(true);
+      }
+    })()
+  }, [currentySelectedPageIndex])
+  
+  if (!loaded) {
+    return <div>Loding...</div>
+  }
+
+  if (errored) {
+    return <div>Sorry, an error occured. Please try refreshing the page.</div>
+  }
+
+  if (countries.length > 0) {
+    
+    return (<div>
+        {countries.map(country => (<div>{country.name}</div>))}
+    </div>)
+  }
+
+  return <div>Sorry, something unexcepted occured. Please try refreshing the page.</div>
 }
 
 export default App;
